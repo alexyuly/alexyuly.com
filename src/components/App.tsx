3888642052e83937cfce4dfb6664ec7f3fd5ecfd
Alex Yuly <alexyuly2@gmail.com>
2020-02-29 17:02:03 -0800

    Adding some content

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,44 @@ interface Props {}
 
 class App extends React.Component<Props> {
   render() {
-    return "Hello, world!";
+    return (
+      <div id="root">
+        <header>
+          <h1>alex yuly</h1>
+          <h2>web designer</h2>
+        </header>
+        <main>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+          <section>
+            <h1>This is the title of some content.</h1>
+            <p>This is the body of some content.</p>
+          </section>
+        </main>
+      </div>
+    );
   }
 }
 
